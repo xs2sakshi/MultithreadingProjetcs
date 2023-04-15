6a7b636958cf8bfaa5f076ab1bfa93c2f5443609
xs2sakshi <xs2sakshi@gmail.com>
2023-04-15 07:59:38 +0530

    Create MainPrintNumber.java

---
+++ MainPrintNumber.java
@@ -0,0 +1,14 @@
+
+public class MainPrintNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<100;i++){
+			NumberPrinter np=new NumberPrinter(i);
+			Thread t = new Thread(np);
+			t.run();
+		}
+
+	}
+
+}
